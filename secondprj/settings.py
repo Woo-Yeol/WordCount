583b0d9f7ec5a59118e98adb8dd1790c2b025804
Woo-Yeol <kkkdnduf@likelion.org>
2020-06-13 22:10:27 +0900

    first commit

--- secondprj/settings.py
+++ secondprj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','@kdng1)uklh!=)fya!clv)-&34j=8e6
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
